f0e3c2cdd7737e440449e2d409608c76bbeb918f
worldstunning <worldstunning2@gmail.com>
2026-01-06 15:37:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,14 @@
+#####################################
+# TERRAFORM & PROVIDER
+#####################################
+
 terraform {
-  required_version = ">= 1.5.0"
+  required_version = ">= 0.13"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.100"
+      version = "4.57.0"
     }
   }
 }
@@ -15,59 +19,40 @@ provider "azurerm" {
 
 data "azurerm_subscription" "current" {}
 
-############################
-# COMMON VARIABLES (INLINE)
-############################
+#####################################
+# LOCALS
+#####################################
 
 locals {
-  location          = "centralindia"
-  resource_group    = "rg-01"
+  location       = "centralindia"
+  resource_group = "rg-01"
 
   tags = {
     "Business Unit" = "IT"
     "Cost Center"   = "CC-1001"
   }
 
-  vnet_id               = "/subscriptions/${data.azurerm_subscription.current.subscription_id}/resourceGroups/rg-01/providers/Microsoft.Network/virtualNetworks/vnet-01"
-  aks_subnet_id         = "${local.vnet_id}/subnets/aks-subnet"
-  private_endpoint_subnet_id = "${local.vnet_id}/subnets/pe-subnet"
-  aci_subnet_id         = "${local.vnet_id}/subnets/aci-subnet"
+  vnet_id   = "/subscriptions/${data.azurerm_subscription.current.subscription_id}/resourceGroups/rg-01/providers/Microsoft.Network/virtualNetworks/vnet-01"
+  aks_subnet_id = "${local.vnet_id}/subnets/aks-subnet"
+  aci_subnet_id = "${local.vnet_id}/subnets/aci-subnet"
+  psql_subnet_id = "${local.vnet_id}/subnets/psql-subnet"
 }
 
-############################
-# AZURE POLICY DEFINITIONS
-############################
+#####################################
+# AZURE POLICIES (CORRECTED)
+#####################################
 
 resource "azurerm_policy_definition" "mandatory_tags" {
   name         = "mandatory-tags"
   policy_type = "Custom"
   mode        = "Indexed"
-  display_name = "Require mandatory tags"
+  display_name = "Require Business Unit and Cost Center tags"
 
   policy_rule = jsonencode({
     if = {
       anyOf = [
         { field = "tags['Business Unit']", exists = "false" },
         { field = "tags['Cost Center']", exists = "false" }
-      ]
-    }
-    then = {
-      effect = "deny"
-    }
-  })
-}
-
-resource "azurerm_policy_definition" "deny_public_ip_nic" {
-  name         = "deny-public-ip-on-nic"
-  policy_type = "Custom"
-  mode        = "Indexed"
-  display_name = "Deny Public IP on NIC"
-
-  policy_rule = jsonencode({
-    if = {
-      allOf = [
-        { field = "type", equals = "Microsoft.Network/networkInterfaces" },
-        { field = "Microsoft.Network/networkInterfaces/ipConfigurations[*].publicIpAddress.id", exists = "true" }
       ]
     }
     then = {
@@ -95,21 +80,21 @@ resource "azurerm_policy_definition" "allowed_locations" {
   })
 }
 
-resource "azurerm_policy_assignment" "subscription_policies" {
-  for_each = {
-    tags      = azurerm_policy_definition.mandatory_tags.id
-    nopip     = azurerm_policy_definition.deny_public_ip_nic.id
-    location  = azurerm_policy_definition.allowed_locations.id
-  }
-
-  name                 = each.key
-  policy_definition_id = each.value
+resource "azurerm_policy_assignment" "mandatory_tags" {
+  name                 = "mandatory-tags"
+  policy_definition_id = azurerm_policy_definition.mandatory_tags.id
   scope                = data.azurerm_subscription.current.id
 }
 
-############################
-# ACR (PRIVATE)
-############################
+resource "azurerm_policy_assignment" "allowed_locations" {
+  name                 = "allowed-locations"
+  policy_definition_id = azurerm_policy_definition.allowed_locations.id
+  scope                = data.azurerm_subscription.current.id
+}
+
+#####################################
+# ACR (PRIVATE NETWORK ACCESS DISABLED)
+#####################################
 
 resource "azurerm_container_registry" "acr" {
   name                = "acrprivate01"
@@ -117,13 +102,15 @@ resource "azurerm_container_registry" "acr" {
   location            = local.location
   sku                 = "Premium"
   admin_enabled       = false
+
   public_network_access_enabled = false
+
   tags = local.tags
 }
 
-############################
-# AKS (PRIVATE)
-############################
+#####################################
+# AKS (PRIVATE CLUSTER – VALID)
+#####################################
 
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = "aks-private-01"
@@ -132,6 +119,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   dns_prefix          = "aksprivate"
 
   private_cluster_enabled = true
+  private_dns_zone_id     = "System"
 
   default_node_pool {
     name           = "system"
@@ -146,29 +134,31 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
   network_profile {
     network_plugin = "azure"
-    outbound_type  = "userDefinedRouting"
+    outbound_type  = "loadBalancer"
   }
 
   tags = local.tags
 }
 
-############################
+#####################################
 # AZURE CONTAINER INSTANCE (PRIVATE)
-############################
+#####################################
 
 resource "azurerm_container_group" "aci" {
   name                = "aci-private-01"
   location            = local.location
   resource_group_name = local.resource_group
-  ip_address_type     = "Private"
   os_type             = "Linux"
-  subnet_ids          = [local.aci_subnet_id]
+
+  ip_address_type = "Private"
+  subnet_ids      = [local.aci_subnet_id]
 
   container {
     name   = "app"
     image  = "mcr.microsoft.com/azuredocs/aci-helloworld"
     cpu    = 1
     memory = 1
+
     ports {
       port     = 80
       protocol = "TCP"
@@ -178,23 +168,24 @@ resource "azurerm_container_group" "aci" {
   tags = local.tags
 }
 
-############################
+#####################################
 # POSTGRESQL FLEXIBLE SERVER (PRIVATE)
-############################
+#####################################
 
 resource "azurerm_postgresql_flexible_server" "psql" {
-  name                   = "psql-flex-01"
-  resource_group_name    = local.resource_group
-  location               = local.location
+  name                = "psql-flex-01"
+  resource_group_name = local.resource_group
+  location            = local.location
+
   administrator_login    = "pgadmin"
   administrator_password = "P@ssword12345!"
   version                = "15"
 
-  delegated_subnet_id = local.private_endpoint_subnet_id
+  delegated_subnet_id = local.psql_subnet_id
   private_dns_zone_id = null
 
-  storage_mb = 32768
   sku_name   = "GP_Standard_D2s_v3"
+  storage_mb = 32768
 
   tags = local.tags
 }
@@ -202,24 +193,24 @@ resource "azurerm_postgresql_flexible_server" "psql" {
 resource "azurerm_postgresql_flexible_server_database" "employee_db" {
   name      = "employee"
   server_id = azurerm_postgresql_flexible_server.psql.id
-  collation = "en_US.utf8"
   charset   = "UTF8"
+  collation = "en_US.utf8"
 }
 
-############################
-# KEY VAULT (PRIVATE)
-############################
+#####################################
+# KEY VAULT (NO PUBLIC ACCESS)
+#####################################
 
 resource "azurerm_key_vault" "kv" {
-  name                        = "kv-private-01"
-  location                    = local.location
-  resource_group_name         = local.resource_group
-  tenant_id                   = data.azurerm_subscription.current.tenant_id
-  sku_name                    = "standard"
-  public_network_access_enabled = false
+  name                = "kv-private-01"
+  location            = local.location
+  resource_group_name = local.resource_group
+  tenant_id           = data.azurerm_subscription.current.tenant_id
+  sku_name            = "standard"
 
-  soft_delete_retention_days = 7
-  purge_protection_enabled  = true
+  public_network_access_enabled = false
+  purge_protection_enabled      = true
+  soft_delete_retention_days    = 7
 
   tags = local.tags
 }
